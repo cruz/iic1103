76a78c8ec0dce54883ee54c6453a6bf26f968de0
Cristian Ruz (hercules) <cruz@ing.puc.cl>
2015-07-09 21:58:35 -0300

    Better square extraction, copied from squares.py

--- scan-IIC1103/scan-scripts/getDigits.py
+++ scan-IIC1103/scan-scripts/getDigits.py
@@ -22,84 +22,90 @@ def cleanTmpDir():
 		if os.path.isfile(TEMP_DIR + "/" + i):
 			os.remove(TEMP_DIR + "/" + i)
 
+def angle_cos(p0, p1, p2):
+	d1, d2 = (p0-p1).astype('float'), (p2-p1).astype('float')
+	return abs(np.dot(d1,d2) / np.sqrt(np.dot(d1,d1)*np.dot(d2,d2)))
 
-def extractDigits(baseName):
+# square extraction idea from /usr/share/doc/opencv-doc/examples/python2/squares.py
+def extractDigits(baseName,debug=False):
 	fileName = INPUT_DIR+"/"+baseName+".png"
 	image = cv2.imread(fileName)
 	print "Read file "+ fileName
-	#convert to gray
-	image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	h,w = image_gray.shape
-	#print "0..." + str(CORNER_HEIGHT) + ", " + str(w-CORNER_WIDTH) + "," + str(w)
-	image_corner = image_gray[0:CORNER_HEIGHT,w-CORNER_WIDTH:w]
-
-	kernel = np.ones((2,2),np.uint8)
-	image_corner = cv2.erode(image_corner,kernel, iterations=1)
-
-	
-	#image_corner = cv2.Laplacian(image_corner,0)
-
-	#image_corner_blurred = cv2.GaussianBlur(image_corner, (0,0), 3)
-	#image_corner = cv2.addWeighted(image_corner, 1.5, image_corner_blurred, -1, 0)
-    #image_corner = 
-
+	#cut corner
+	h,w,c = image.shape
 	print "Original:" + str(image.shape)
-	print "Grayed:  " + str(image_gray.shape)
+	
+	image_corner = image[0:CORNER_HEIGHT,w-CORNER_WIDTH:w]
 	print "Cut:     " + str(image_corner.shape)
+	#convert to gray
+	image_gray = cv2.cvtColor(image_corner, cv2.COLOR_BGR2GRAY)
+	print "Grayed:  " + str(image_gray.shape)
+	cv2.imwrite(TEMP_DIR+"/"+baseName+"-corner"+".png", image_corner)
+	cv2.imwrite(TEMP_DIR+"/"+baseName+"-gray"+".png", image_gray)
 
-	cv2.imwrite(TEMP_DIR+"/"+baseName+"-bw"+".png", image_gray)
-	cv2.imwrite(TEMP_DIR+"/"+baseName+"-cut"+".png", image_corner)
+	image_blur = cv2.GaussianBlur(image_gray, (5,5), 0)
+	image_bluc = cv2.Canny(image_blur,0,50,apertureSize=5)
+	image_blcd = cv2.dilate(image_bluc, None )
+
+	if debug:
+		cv2.imshow('image_corner', image_corner)
+		ch = 0xFF & cv2.waitKey()
+		cv2.imshow('image_gray', image_gray)
+		ch = 0xFF & cv2.waitKey()
+		cv2.imshow('image_blur', image_blur)
+		ch = 0xFF & cv2.waitKey()
+		cv2.imshow('image_bluc', image_bluc)
+		ch = 0xFF & cv2.waitKey()
+		cv2.imshow('image_blcd', image_blcd)
+		ch = 0xFF & cv2.waitKey()
 
 
+
+
+	#retval,image_bin = cv2.threshold(image_blur,0,255,cv2.THRESH_BINARY)
 	nSquare = 0
-	#from: http://stackoverflow.com/questions/11424002/how-to-detect-simple-geometric-shapes-using-opencv
-	ret,image_bin = cv2.threshold(image_corner,127,255,1)
-	cv2.imwrite(TEMP_DIR+"/"+baseName+"-bin"+".png", image_bin)
-	#image_bin = image_corner
-	print "Binary:  " + str(image_bin.shape)
-	contours,h = cv2.findContours(image_bin,1,2)
+	squares = []
+	rects = []
+	contours, hierarchy = cv2.findContours(image_blcd, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 	for cnt in contours:
-		#approximate curves
-		approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
-		#print len(approx)
-		# Take into account only squares
-		if len(approx)==4:
-			squareArea = cv2.contourArea(cnt)
-			#print "square " + " ... " + str(approx)
-			#print "square " + " ... " + str(approx[0][0][0]) + ", " + str(approx[0][0][1])
-			if(squareArea >= AREA_THRESHOLD_MIN and squareArea <= AREA_THRESHOLD_MAX):
-				#for i in range(0,10):
-				#	print str(image_gray[approx[0][0][0]+i,approx[0][0][1]:approx[0][0][1]+10])
-				M = cv2.moments(cnt)
-				x,y,w,h = cv2.boundingRect(approx)
-				s = 0
-				s0 = 0
-				s1 = 0
-				#for i in range(x,x+w):
-				#	for j in range(y,y+h):
-				#		s += image_bin[i,j]
-				#		if image_bin[i,j] == 0: s0 += 1
-				#		else: s1 += 1
-				print "square " + str(nSquare) + ", " + str(squareArea) + ", rect: " + str(x)+","+str(y)+","+str(w)+","+str(h)+ ", area " + str(w*h) + ", Sum:" + str(s) + ", 0s:" + str(s0) + ", 1s:" + str(s1)
-
+		cnt_len = cv2.arcLength(cnt,True)
+		cnt = cv2.approxPolyDP(cnt, 0.02*cnt_len, True)
+		if(len(cnt) == 4 and cv2.contourArea(cnt) > AREA_THRESHOLD_MIN and cv2.contourArea(cnt) < AREA_THRESHOLD_MAX and cv2.isContourConvex(cnt)):
+			x,y,w,h = cv2.boundingRect(cnt)
+			cnt = cnt.reshape(-1,2)
+			max_cos = np.max([angle_cos( cnt[i], cnt[(i+1)%4], cnt[(i+2)%4]) for i in xrange(4)])
+			if max_cos < 0.1:
+				squareArea = cv2.contourArea(cnt)
+				print "square " + str(nSquare) + ", " + str(squareArea) + ", rect: " + str(x)+","+str(y)+","+str(w)+","+str(h)+ ", area " + str(w*h)
+				squares.append(cnt)
 				#extract cut
-				square = image_corner[y:y+h,x:x+w]
-				cv2.imwrite(TEMP_DIR+"/"+baseName+"-"+str(nSquare)+".png", square)
-				#cv2.drawContours(image_corner,[cnt],0,(0,0,255),-1)
-				
+				rect = image_corner[y:y+h,x:x+w]
+				cv2.imwrite(TEMP_DIR+"/"+baseName+"-"+str(nSquare)+".png", rect)
 				nSquare += 1
 
-		#TODO: store square
-		#TODO: sort squares
-		#TODO: process square		
+	#display squares
+	print "Found " + str(len(squares)) + " squares"
 
-	cv2.imwrite(TEMP_DIR+"/"+baseName+"-cut"+".png", image_corner)
-				
-	cv2.imwrite(TEMP_DIR+"/"+baseName+"-cont"+".png", image)
-	#cv2.imwrite(TEMP_DIR+"/"+baseName+"-bin"+".png", image_bin)
+	cv2.drawContours(image_corner, squares, -1, (0,255,0), 3)
+	cv2.imwrite(TEMP_DIR+"/"+baseName+"-corner-squares"+".png", image_corner)
+
+	if debug:
+		cv2.imshow('squares', image_corner)
+		ch = 0xFF & cv2.waitKey()
+	
+		# for square in squares:
+		# 	image = cv2.imread(fileName)
+		# 	image_corner = image[0:CORNER_HEIGHT,w-CORNER_WIDTH:w]
+		# 	cv2.drawContours(image_corner, [square], -1, (0,255,0), 3)
+		# 	cv2.imshow('one square', image_corner)
+		# 	ch = 0xFF & cv2.waitKey()
+		# 	if ch == 27:
+		# 		break
 
 
-
+		#if ch == 27:
+		#	break
+		cv2.destroyAllWindows()
 
 
 
@@ -113,7 +119,11 @@ def main():
 		ext = inputFile[inputFile.rfind('.')+1:]
 		if ext.lower() == "png":
 			print "Processing " + INPUT_DIR + "/" + inputFile
-			extractDigits(baseName)
+			extractDigits(baseName,debug=False)
+
+	cv2.destroyAllWindows()
+
+
 
 if __name__ == "__main__":
 	main()
